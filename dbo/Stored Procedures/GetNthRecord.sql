38c6ea523c9f46d0d03e4470ecd855b85ba9c5f6
Shiv <shshivdatt@hcl.com>
2022-04-30 21:14:51 +0530

    First commit

---
+++ dbo/Stored Procedures/GetNthRecord.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROC GetNthRecord
+AS
+
+DECLARE @nth INT=0
+SET @nth=3
+--SELECT  e.first_name,e.last_name,d.department_name,salary,
+--        ROW_NUMBER() OVER (
+--PARTITION BY d.id ORDER BY salary DESC
+--) AS salary_rank
+--FROM department d JOIN employee e ON d.id = e.department_id
+--ORDER BY department_name,salary DESC;
+
+
+SELECT * FROM (
+SELECT  e.first_name,e.last_name,d.department_name,salary,ROW_NUMBER() OVER (PARTITION BY d.id ORDER BY salary DESC) AS salary_rank
+FROM department d JOIN employee e ON d.id = e.department_id
+) tblA WHERE salary_rank=@nth
---
+++ dbo/Tables/Employee.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Employee] (
+    [Id]            INT           IDENTITY (1, 1) NOT NULL,
+    [first_name]    NVARCHAR (50) NULL,
+    [last_name]     NVARCHAR (50) NULL,
+    [salary]        FLOAT (53)    NULL,
+    [department_id] INT           NULL,
+    [Gender]        NVARCHAR (50) NULL,
+    CONSTRAINT [PK_Employee] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
